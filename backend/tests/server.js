a43a5371e13360018a917181f80ee29610eb4bc7
Iddhi Dasanayaka <iddhidasanayaka@gmail.com>
2023-12-26 01:31:05 +0530

    Change database conneciton

--- backend/tests/server.js
+++ backend/tests/server.js
@@ -5,12 +5,15 @@ import routes from "../configs/routes.js";
 
 // initialize environment variables
 envConfig();
-
+// initialize database connection
+const databaseString = process.env.NODE_ENV
+    ? `${process.env.DB}/${process.env.NODE_ENV}?retryWrites=true&w=majority`
+    : process.env.DB;
 // connect to database
-mongoose.connect(process.env.DB, {
-    writeConcern: { w: "majority" },
+mongoose.connect(databaseString).catch((ex) => {
+    console.error("Failed to connect to MongoDB");
+    process.exit(1);
 });
-
 // intialize application
 const app = express();
 
--- backend/tests/serverSocket.js
+++ backend/tests/serverSocket.js
@@ -11,14 +11,10 @@ const databaseString = process.env.NODE_ENV
     ? `${process.env.DB}/${process.env.NODE_ENV}?retryWrites=true&w=majority`
     : process.env.DB;
 
-mongoose
-    .connect(databaseString, {
-        writeConcern: { w: "majority" },
-    })
-    .catch((ex) => {
-        console.error("Failed to connect to MongoDB");
-        process.exit(1);
-    });
+mongoose.connect(databaseString).catch((ex) => {
+    console.error("Failed to connect to MongoDB");
+    process.exit(1);
+});
 const server = initWebSocket(app);
 
 export default server;
