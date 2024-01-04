bfe1a8f48bf5ce046470dd2c3c02f3b7c9660cc2
Pramod Mannapperuma <96408353+PramodMannapperuma@users.noreply.github.com>
2024-01-04 19:40:43 +0530

    Auth Tests

---
+++ backend/tests/integration/authRoute.test.js
@@ -0,0 +1,75 @@
+import request from "supertest";
+import server, { db } from "../server";
+import UserModel from "../../models/users";
+import mongoose from "mongoose";
+// import jwt from "jsonwebtoken";
+
+describe("User Login Integration Tests", () => {
+  afterEach(async () => {
+    await UserModel.deleteMany();
+  });
+  afterAll(() => {
+    mongoose.disconnect();
+  });
+  const userData = {
+    name: "badda",
+    email: "badda@gmail.com",
+    password: "123456",
+  };
+
+  describe("POST api/Login", () => {
+    it("should login an existing user instance", async () => {
+      const user = new UserModel(userData);
+      await user.save();
+      const response = await request(server)
+        .post("/api/Login")
+        .send(userData)
+        .expect(200);
+      expect(response.text).toEqual("Successfully logged in");
+      console.log(response.body);
+    });
+    it("Should return jwt token", async () => {
+      const user = new UserModel(userData);
+      await user.save();
+      const response = await request(server)
+        .post("/api/Login")
+        .send(userData)
+        .expect(200);
+      expect(response.headers["x-auth-token"]).toBeDefined();
+      console.log(response.body);
+    });
+    it("Should return 400 for invalid password", async () => {
+      const user = new UserModel(userData);
+      await user.save();
+
+      const WronguserData = {
+        name: "badda",
+        email: "badda@gmail.com",
+        password: "12345",
+      };
+
+      const response = await request(server)
+        .post("/api/Login")
+        .send(WronguserData)
+        .expect(400);
+      expect(response.body).toBe("The password dosent match");
+    });
+
+    it("Should return 400 if user dosen't exist", async () => {
+      const user = new UserModel(userData);
+      await user.save();
+
+      const WronguserData = {
+        name: "badda",
+        email: "badd@gmail.com",
+        password: "1234567",
+      };
+
+      const response = await request(server)
+        .post("/api/Login")
+        .send(WronguserData)
+        .expect(400);
+      expect(response.body).toBe("The user dosent exist");
+    });
+  });
+});
