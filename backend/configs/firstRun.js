6336d5fd5e883cec76843a067b6b976723a0c355
Iddhi Dasanayaka <iddhidasanayaka@gmail.com>
2024-01-05 00:04:53 +0530

    fix first run bug

--- backend/configs/firstRun.js
+++ backend/configs/firstRun.js
@@ -1,5 +1,6 @@
 import { Chat } from "../models/Chat.js";
 import { ChatUser } from "../models/ChatUser.js";
+import UserModel from "../models/users.js";
 import { Message } from "../models/Message.js";
 import { Staff } from "../models/staff.js";
 import { encrypt } from "../utils/hash.js";
@@ -11,6 +12,7 @@ async function DeleteAll() {
     await ChatUser.deleteMany({});
     await Chat.deleteMany({});
     await Message.deleteMany({});
+    await UserModel.deleteMany({});
 }
 async function FirstRun() {
     const hashedPassword = await encrypt("admin");
@@ -25,11 +27,27 @@ async function FirstRun() {
     });
     await staff.save();
     const chatUser = new ChatUser({
+        _id: staff._id,
         name: staff.firstname + " " + staff.lastname,
         avatar: staff.image,
         isAdmin: true,
     });
     await chatUser.save();
+
+    // insert default user to database
+    const user = new UserModel({
+        name: "User",
+        email: "asd1@asd.com",
+        password: "asdasd",
+    });
+    await user.save();
+    const chatUser2 = new ChatUser({
+        _id: user._id,
+        name: user.name,
+        avatar: Math.random(),
+        isAdmin: false,
+    });
+    await chatUser2.save();
 }
 
 export { FirstRun, DeleteAll };
