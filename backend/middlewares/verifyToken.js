44cd1dafba54e7a9946e08162c7e2e3119191eed
Iddhi Dasanayaka <iddhidasanayaka@gmail.com>
2024-01-07 15:29:00 +0530

    Fix JWT token expire crash issue

--- backend/middlewares/verifyToken.js
+++ backend/middlewares/verifyToken.js
@@ -6,16 +6,19 @@ function verifyToken(req, res, next) {
     if (!token) {
         return res.status(403).json({ message: "Token not provided" });
     }
+    try {
+        jwt.verify(token, process.env.secretKey, (err, decoded) => {
+            if (err) {
+                return res.status(401).json({ message: "Unauthorized" });
+            }
 
-    jwt.verify(token, process.env.secretKey, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ message: "Unauthorized" });
-        }
-
-        // Attach the decoded user information to the request object for use in subsequent middleware or routes
-        req.user = decoded;
-        next();
-    });
+            // Attach the decoded user information to the request object for use in subsequent middleware or routes
+            req.user = decoded;
+            next();
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 }
 
 module.exports = verifyToken;
